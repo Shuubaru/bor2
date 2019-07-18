3928fab144f5e34704c77db14f49a2adbdad3116
Shuubaru <43148549+Shuubaru@users.noreply.github.com>
2019-07-18 00:54:25 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,4 @@
 http = require("http")
-class = require('class'),
-enums = require('enums'),
-extensions = require('extensions'),
-package = require('./package.lua'),
-Client = require('client/Client'),
-Clock = require('utils/Clock'),
-Color = require('utils/Color'),
-Date = require('utils/Date'),
-Deque = require('utils/Deque'),
-Emitter = require('utils/Emitter'),
-Logger = require('utils/Logger'),
-Mutex = require('utils/Mutex'),
-Permissions = require('utils/Permissions'),
-Stopwatch = require('utils/Stopwatch'),
-Time = require('utils/Time'),
-storage = {},
 
 -- "A web dyno must bind to its assigned $PORT within 60 seconds of startup."
 -- see https://devcenter.heroku.com/articles/dynos#web-dynos
@@ -31,19 +15,4 @@ end):listen(port)
 print("Server listening on port "..port)
 
 
-local discordia = require('discordia')
-local cl = discordia.Client()
-
 print("test present")
-
-cl:on('ready', function()
-	print('Logged in as '.. cl.user.username)
-end)
-
-cl:on('messageCreate', function(message)
-	if message.content == '!ping' then
-		message.channel:send('Pong!')
-	end
-end)
-
-cl:run('Bot NjAxMjIzNjg5MDg5OTc0MzE2.XS_LYg.OOjDifoAKjf0fVWm9mVEDqo-hew')
