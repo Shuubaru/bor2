6f339fca36126965539f9ac63b4aa6c2ed7b8b3e
Shuubaru <43148549+Shuubaru@users.noreply.github.com>
2019-07-18 00:42:42 -0300

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,8 +1,25 @@
-return {
+--[[return {
   name = "heroku-luvit/luvit-example",
   version = "1.0.0",
   description = "Example luvit app for heroku",
   dependencies = {
     "luvit/luvit"
   },
+}]]--
+
+return {
+  name = "heroku-luvit/luvit-example",
+  version = "1.0.0",
+  homepage = 'https://github.com/SinisterRectus/Discordia',
+  description = "Example luvit app for heroku",
+  dependencies = {
+    "luvit/luvit",
+		'creationix/coro-http@3.1.0',
+		'creationix/coro-websocket@3.1.0',
+		'luvit/secure-socket@1.2.2',
+  },
+	tags = {'discord', 'api'},
+	license = 'MIT',
+	author = 'Sinister Rectus',
+	files = {'**.lua'},
 }
