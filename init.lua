4242252ffc106eb23bfe1ab4d4cbd1c9df20d08e
Shuubaru <43148549+Shuubaru@users.noreply.github.com>
2019-07-18 00:40:21 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
-local http = require("http")
+http = require("http")
+class = require('class'),
+enums = require('enums'),
+extensions = require('extensions'),
+package = require('./package.lua'),
+Client = require('client/Client'),
+Clock = require('utils/Clock'),
+Color = require('utils/Color'),
+Date = require('utils/Date'),
+Deque = require('utils/Deque'),
+Emitter = require('utils/Emitter'),
+Logger = require('utils/Logger'),
+Mutex = require('utils/Mutex'),
+Permissions = require('utils/Permissions'),
+Stopwatch = require('utils/Stopwatch'),
+Time = require('utils/Time'),
+storage = {},
 
 -- "A web dyno must bind to its assigned $PORT within 60 seconds of startup."
 -- see https://devcenter.heroku.com/articles/dynos#web-dynos
